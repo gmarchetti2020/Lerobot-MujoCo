aa6253ab5d0472c60b350b2b4aaedd5ff047580e
Jeonguen <baro0906@kore.aac.kr>
2026-01-13 16:47:26 +0900

    eval

--- download_data.py
+++ download_data.py
@@ -1,3 +1,12 @@
-from lerobot.datasets.lerobot_dataset import LeRobotDataset
-root = './dataset/leader_data'
-dataset = LeRobotDataset('Jeongeun/tutorial_v2',root = root )
+# from lerobot.datasets.lerobot_dataset import LeRobotDataset
+# root = './dataset/leader_data'
+# dataset = LeRobotDataset('Jeongeun/tutorial_v2',root = root )
+
+from lerobot.policies.pi05.modeling_pi05 import PI05Policy
+repo_id_or_path = 'Jeongeun/tutorial_v2_pi05' # Use this for loading pretrained model from the hub
+device = 'cuda'
+
+# dataset_metadata = LeRobotDatasetMetadata("Jeongeun/tutorial_v2", root=ROOT)
+
+policy = PI05Policy.from_pretrained(repo_id_or_path)
+policy.to(device)
